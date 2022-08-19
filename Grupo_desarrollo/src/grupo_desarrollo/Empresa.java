45d09fe2c0ce4f3daae0c8d55baae30e972a5e9d
David1585 <100976020+david1585@users.noreply.github.com>
2022-08-19 10:08:55 -0500

    Se cran los atríbutos y métodos de la clase empresa

---
+++ Grupo_desarrollo/src/grupo_desarrollo/Empresa.java
@@ -0,0 +1,50 @@
+package grupo_desarrollo;
+
+public class Empresa {
+    // Atríbutos
+    private String Nombre;
+    private String Direccion;
+    private String Telefono;
+    private String Nit;
+
+    // Método constructor
+    public Empresa(String nombre, String direccion, String telefono, String nit) {
+        Nombre = nombre;
+        Direccion = direccion;
+        Telefono = telefono;
+        Nit = nit;
+    }
+
+    // Getter anb Setter
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        Telefono = telefono;
+    }
+
+    public String getNit() {
+        return Nit;
+    }
+
+    public void setNit(String nit) {
+        Nit = nit;
+    }
+}
